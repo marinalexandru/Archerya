eb34eb0452456946306b4864d21b7cb32cd56eda
Alexandru Marin <marin.alexandru.andu@gmail.com>
2018-07-26 01:48:51 +0300

    Added starter 2 kit for nature and created map paths and shrines

--- Assets/Scripts/AttackBehaviour.cs
+++ Assets/Scripts/AttackBehaviour.cs
@@ -67,13 +67,4 @@ public class AttackBehaviour : StateMachineBehaviour
         animator.speed = InitialSpeed;
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
 }
